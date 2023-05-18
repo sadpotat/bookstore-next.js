163705f185769c4543f493c517f30d0937d115d8
sadpotat <sadia.tasneem16@gmail.com>
2023-05-18 10:36:55 +0600

    added books/[id] that will contain book details

---
+++ bookstore3/components/BookDetail.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BookDetail = () => {
+    return <h1 style={{ color: "black" }}>Book details</h1>;
+};
+
+export default BookDetail;
--- bookstore3/pages/books/[id].js
+++ bookstore3/pages/books/[id].js
@@ -1,3 +1,18 @@
-export default function BookDetails() {
-    return <h1>Book details</h1>;
+import BookDetail from "@/components/BookDetail";
+
+export default function detailsPage() {
+    return (
+        <div
+            style={{
+                backgroundColor: "white",
+                position: "absolute",
+                top: "63px",
+                right: "0px",
+                bottom: "0px",
+                left: "0px",
+            }}
+        >
+            <BookDetail />
+        </div>
+    );
 }
