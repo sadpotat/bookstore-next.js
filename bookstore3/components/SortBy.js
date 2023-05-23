1d73b72e9aad91dc052392101854db9c809baf77
sadpotat <sadia.tasneem16@gmail.com>
2023-05-23 23:34:47 +0600

    added sort component

---
+++ bookstore3/components/SortBy.js
@@ -0,0 +1,122 @@
+import {
+    Button,
+    ClickAwayListener,
+    Grow,
+    MenuItem,
+    MenuList,
+    Paper,
+    Popper,
+} from "@mui/material";
+import React, { useEffect, useRef, useState } from "react";
+
+const SortBy = function ({ onSubmit }) {
+    const [open, setOpen] = useState(false);
+    const anchorRef = useRef(null);
+
+    const handleToggle = () => {
+        setOpen((prevOpen) => !prevOpen);
+    };
+
+    const handleClose = (event) => {
+        if (anchorRef.current && anchorRef.current.contains(event.target)) {
+            return;
+        }
+
+        setOpen(false);
+    };
+
+    const handleTitle = () => {
+        onSubmit("Title");
+        setOpen(false);
+    };
+    const handleAuthor = () => {
+        onSubmit("Author");
+        setOpen(false);
+    };
+    const handlePrice = () => {
+        onSubmit("Price");
+        setOpen(false);
+    };
+
+    function handleListKeyDown(event) {
+        if (event.key === "Tab") {
+            event.preventDefault();
+            setOpen(false);
+        } else if (event.key === "Escape") {
+            setOpen(false);
+        }
+    }
+
+    // return focus to the button when we transitioned from !open -> open
+    const prevOpen = React.useRef(open);
+    useEffect(() => {
+        if (prevOpen.current === true && open === false) {
+            anchorRef.current.focus();
+        }
+
+        prevOpen.current = open;
+    }, [open]);
+    return (
+        <div>
+            <Button
+                ref={anchorRef}
+                id="composition-button"
+                aria-controls={open ? "composition-menu" : undefined}
+                aria-expanded={open ? "true" : undefined}
+                aria-haspopup="true"
+                onClick={handleToggle}
+                sx={{
+                    fontSize: "16px",
+                    border: "1px solid teal",
+                    color: "teal",
+                }}
+            >
+                Sort By
+            </Button>
+            <Popper
+                open={open}
+                anchorEl={anchorRef.current}
+                role={undefined}
+                placement="bottom-start"
+                transition
+                disablePortal
+                sx={{ zIndex: 10 }}
+            >
+                {({ TransitionProps, placement }) => (
+                    <Grow
+                        {...TransitionProps}
+                        style={{
+                            transformOrigin:
+                                placement === "bottom-start"
+                                    ? "left top"
+                                    : "left bottom",
+                        }}
+                    >
+                        <Paper>
+                            <ClickAwayListener onClickAway={handleClose}>
+                                <MenuList
+                                    autoFocusItem={open}
+                                    id="composition-menu"
+                                    aria-labelledby="composition-button"
+                                    onKeyDown={handleListKeyDown}
+                                >
+                                    <MenuItem onClick={handleTitle}>
+                                        Title
+                                    </MenuItem>
+                                    <MenuItem onClick={handleAuthor}>
+                                        Author
+                                    </MenuItem>
+                                    <MenuItem onClick={handlePrice}>
+                                        Price
+                                    </MenuItem>
+                                </MenuList>
+                            </ClickAwayListener>
+                        </Paper>
+                    </Grow>
+                )}
+            </Popper>
+        </div>
+    );
+};
+
+export default SortBy;
