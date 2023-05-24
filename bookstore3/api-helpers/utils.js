4a13e1d25b64cc8cd8acd76f82afc787e146fdb5
sadpotat <sadia.tasneem16@gmail.com>
2023-05-24 12:27:28 +0600

    added a user with only "find" permissions to api-helpers/utils.js.

--- bookstore3/api-helpers/utils.js
+++ bookstore3/api-helpers/utils.js
@@ -1,18 +1,18 @@
 const { MongoKerberosError } = require("mongodb");
 import mongoose from "mongoose";
 
-// loading credentials
-import fsPromises from "fs/promises";
-import path from "path";
-export async function getCredentials() {
-    const filePath = path.join(process.cwd(), "api-helpers/credentials.json");
-    const jsonData = await fsPromises.readFile(filePath);
-    const credentials = JSON.parse(jsonData);
-    return credentials;
-}
+// // loading credentials
+// import fsPromises from "fs/promises";
+// import path from "path";
+// export async function getCredentials() {
+//     const filePath = path.join(process.cwd(), "api-helpers/credentials.json");
+//     const jsonData = await fsPromises.readFile(filePath);
+//     const credentials = JSON.parse(jsonData);
+//     return credentials;
+// }
 
 export const connectToDatabase = async () => {
-    const credentials = await getCredentials();
+    // const credentials = await getCredentials();
 
     // returns without calling mongoose.connect() if the
     //connection status is 1: connected or 2: connecting
@@ -25,7 +25,7 @@ export const connectToDatabase = async () => {
 
     await mongoose
         .connect(
-            `mongodb+srv://${credentials.user}:${credentials.password}@cluster0.etwhyuv.mongodb.net/?retryWrites=true&w=majority?connectTimeoutMS=300000`
+            `mongodb+srv://bookstoreNextjsViewer:FtkSiKtUst4IkqGb@cluster0.etwhyuv.mongodb.net/?retryWrites=true&w=majority?connectTimeoutMS=300000`
         )
         .then(() => console.log("Connected to mongoDB"))
         .catch((err) => console.log(err));
