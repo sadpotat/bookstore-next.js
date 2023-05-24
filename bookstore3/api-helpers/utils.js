b179a645deabe3926fc1c4e2459ad82c43fc49cb
sadpotat <sadia.tasneem16@gmail.com>
2023-05-24 11:55:43 +0600

    hid database uri

--- bookstore3/api-helpers/utils.js
+++ bookstore3/api-helpers/utils.js
@@ -1,10 +1,22 @@
 const { MongoKerberosError } = require("mongodb");
 import mongoose from "mongoose";
 
+// loading credentials
+import fsPromises from "fs/promises";
+import path from "path";
+export async function getCredentials() {
+    const filePath = path.join(process.cwd(), "api-helpers/credentials.json");
+    const jsonData = await fsPromises.readFile(filePath);
+    const credentials = JSON.parse(jsonData);
+    return credentials;
+}
+
 export const connectToDatabase = async () => {
-    console.log("ready state", mongoose.connection.readyState);
+    const credentials = await getCredentials();
+
     // returns without calling mongoose.connect() if the
     //connection status is 1: connected or 2: connecting
+    console.log("ready state before calling", mongoose.connection.readyState);
     if (
         mongoose.connection.readyState === 1 ||
         mongoose.connection.readyState === 2
@@ -13,7 +25,7 @@ export const connectToDatabase = async () => {
 
     await mongoose
         .connect(
-            "mongodb+srv://sadiatasneem16:sYQeTn1FMcyOZBFZ@cluster0.etwhyuv.mongodb.net/?retryWrites=true&w=majority?connectTimeoutMS=300000"
+            `mongodb+srv://${credentials.user}:${credentials.password}@cluster0.etwhyuv.mongodb.net/?retryWrites=true&w=majority?connectTimeoutMS=300000`
         )
         .then(() => console.log("Connected to mongoDB"))
         .catch((err) => console.log(err));
